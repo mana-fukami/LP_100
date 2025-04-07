19074daa27c2dce2f6c0672d6bcf276c97393394
Fukami Mana <f2210543@gl.cc.uec.ac.jp>
2025-04-08 02:08:40 +0900

    adjust to ver.2025

--- 1/1-2.py
+++ 1/1-2.py
@@ -1,20 +1,11 @@
 #名前に情報を埋め込む
 #誤解されない名前をつける
 
-#「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
+#文字列”stressed”の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
 
-original_text1="パトカー"
-original_text2="タクシー"
-interleaved_text="" #interleave:動作を交互に行う
-length_of_interleaved_text=len(original_text1)+len( original_text2)
+original_text="stressed"
+reversed_text=""
 
-id_of_text1=0
-id_of_text2=0
-for i in range(0,length_of_interleaved_text,1):
-    if i%2==0:
-        interleaved_text+=original_text1[id_of_text1]
-        id_of_text1+=1
-    else:
-        interleaved_text+= original_text2[id_of_text2]
-        id_of_text2+=1
-print(interleaved_text)
+for i in range(len(original_text)-1,-1,-1):
+    reversed_text+=original_text[i]
+print(reversed_text)
