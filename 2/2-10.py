6fce0dfeb99f246be87f50f97a47bf09ae4f790f
Fukami Mana <f2210543@gl.cc.uec.ac.jp>
2025-04-21 09:36:32 +0900

    実行結果を追加

--- 2/2-10.py
+++ 2/2-10.py
@@ -1,11 +1,19 @@
+#美しさ
+#コメントすべき事を知る
+#コメントは正確で簡潔に
 
 original_file=open("2\popular-names.txt","r")
 
 #2-10
-#行数をカウントする
+#行数(改行="\n")をカウントする
 lines=original_file.readlines()
 count_lines=len(lines)
 print("行数："+str(count_lines))
 
-#確認
+# 実行結果
+# 行数：2780
+
+# 確認
 # wc -l "popular-names.txt"
+# 実行結果
+# 2780 popular-names.txt
--- 2/2-16.py
+++ 2/2-16.py
@@ -1,3 +1,7 @@
+#美しさ
+#コメントすべき事を知る
+#コメントは正確で簡潔に
+
 #ファイルを行単位でランダムに並び替えよ
 # （注意: 各行の内容は変更せずに並び替えよ）。
 # 同様の処理をshufコマンドで実現せよ。
@@ -8,6 +12,6 @@ lines= original_file.readlines()
 new_file= open("popular-names-random.txt", "w")
 
 import random
-new_file.writelines(random.sample(lines, len(lines)))
+new_file.writelines(random.sample(lines, len(lines))) #元のファイルの情報を保持したまま、行をランダムに並び替える。
 
 #確認コマンド# shuf popular-names.txt > popular-names-random.txt
