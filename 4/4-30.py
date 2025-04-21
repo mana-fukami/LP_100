52a8f238c9c86323cf62a13941ea6bdbcc9b95c1
mana-fukami <3kan.me6n@gmail.com>
2025-04-21 09:40:33 +0900

    途中経過

--- 4/4-30.py
+++ 4/4-30.py
@@ -8,3 +8,26 @@ text = """
 笛を吹き、羊と遊んで暮して来た。
 けれども邪悪に対しては、人一倍に敏感であった。
 """
+
+#形態素解析に使うライブラリをインポートする
+import MeCab
+import UniDic
+tagger=MeCab.Tagger()
+nodes=tagger.parseToNode(text) #各形態素をノードとして取得する#ノード＝構造体リスト
+#ノードの構成：
+#   node.surface - 表層形
+#   node.feature - 品詞、活用、原型をカンマ区切りで並べた文字列
+#   node.next - 次のノード
+#   node.stat - BOS/EOSなどのステータス
+#   node.length - 形態素の文字数
+#   node.rlength - 表示される長さ
+
+verb=[]
+while nodes:
+    if nodes.surface != "":
+        node_material=nodes.feature.split(",")
+        if node_material[0]=="動詞":
+            verb.append(nodes.surface)
+    nodes=nodes.next
+
+print(verb)
