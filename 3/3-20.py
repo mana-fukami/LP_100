cfcce7c2c6cc7e7bd620f26074a448a311bae157
mana-fukami <3kan.me6n@gmail.com>
2025-04-28 17:24:03 +0900

    実行例の追記

--- 3/3-20.py
+++ 3/3-20.py
@@ -1,3 +1,6 @@
+#制御フローを読みやすくする
+#巨大な式を分割する
+
 #1行に1記事の情報がJSON形式で格納される
 #各行には記事名が”title”キーに、記事本文が”text”キーの辞書オブジェクトに格納
 #ファイル全体はgzipで圧縮される
@@ -7,7 +10,7 @@
 
 import json
 
-json_file=open("jawiki-country.json","r",encoding="utf-8")
+json_file=open("3\jawiki-country.json","r",encoding="utf-8")
 json_lines=json_file.readlines()
 articles=[]
 for line in json_lines:
@@ -19,3 +22,20 @@ for article in articles:
         break
 
 print(UK_text)
+
+#実行結果(一部抜粋)
+"""
+~~~~~
+{{EU|1973年 - 2020年}}
+{{CPLP}}
+{{デフォルトソート:いきりす}}
+[[Category:イギリス|*]]
+[[Category:イギリス連邦加盟国]]
+[[Category:英連邦王国|*]]
+[[Category:G8加盟国]]
+[[Category:欧州連合加盟国|元]]
+[[Category:海洋国家]]
+[[Category:現存する君主国]]
+[[Category:島国]]
+[[Category:1801年に成立した国家・領域]]
+"""
--- 3/3-21.py
+++ 3/3-21.py
@@ -1,3 +1,6 @@
+#制御フローを読みやすくする
+#巨大な式を分割する
+
 #記事中でカテゴリ名を宣言している行を抽出せよ。
 #カテゴリーの形式
 # \n[[Category:エジプト|*]]\n[[Category:共和国]]\n[[Category:軍事政権]]
@@ -5,7 +8,7 @@
 import json
 import re
 
-json_file=open("jawiki-country.json","r",encoding="utf-8")
+json_file=open("3\jawiki-country.json","r",encoding="utf-8")
 json_lines=json_file.readlines()
 articles=[]
 for line in json_lines:
@@ -20,3 +23,8 @@ categories=[]
 categories=re.findall("\[\[Category:.*?\]\]",UK_text)
 
 print(categories)
+
+#実行結果
+"""
+['[[Category:イギリス|*]]', '[[Category:イギリス連邦加盟国]]', '[[Category:英連邦王国|*]]', '[[Category:G8加盟国]]', '[[Category:欧州連合加盟国|元]]', '[[Category:海洋国家]]', '[[Category:現存する君主国]]', '[[Category:島国]]', '[[Category:1801年に成立した国家・領域]]']
+"""
