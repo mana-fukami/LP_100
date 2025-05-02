a1ca3b5fb3ea2abf26feb6efb1ee60ffff5d4ffa
mana-fukami <3kan.me6n@gmail.com>
2025-05-02 15:32:08 +0900

    complete

---
+++ 6/6-51.py
@@ -0,0 +1,22 @@
+# 単語の類似度
+# “United States”と”U.S.”のコサイン類似度を計算せよ。
+
+from WordVector import word2vec
+import numpy as np
+
+word_vec=word2vec()
+
+vec1=word_vec.get_vector("United States")
+vec2=word_vec.get_vector("U.S.")
+
+dot_product=np.dot(vec1,vec2)
+norm_vec1=np.linalg.norm(vec1)
+norm_vec2=np.linalg.norm(vec2)
+cosine_sim=dot_product/(norm_vec1*norm_vec2)
+
+print(f"cosine similarity: {cosine_sim}")
+
+#実行結果
+"""
+cosine similarity: 0.7310774922370911
+"""
