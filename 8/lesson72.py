2578a82a4c5689a7da53eb37efd2f0a8e1a80792
mana-fukami <3kan.me6n@gmail.com>
2025-06-02 15:07:15 +0900

    update

--- 8/lesson72.py
+++ 8/lesson72.py
@@ -10,6 +10,61 @@ from torch import nn
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from lesson70 import embedding_matrix,token_to_id,id_to_token
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction import DictVectorizer
+
+train_tsv=open("SST-2/train.tsv","r")
+train_df=pd.read_csv(train_tsv,sep="\t")
+
+dev_tsv=open("SST-2/dev.tsv","r")
+dev_df=pd.read_csv(dev_tsv,sep="\t")
+
+class CustomDataset(Dataset):
+    def __init__(self,data_df):
+        token_id=self.text_to_token_id(data_df)
+        self.token_id_feature=self.add_average_vec(token_id)
+
+    def __len__(self):
+        return len(self.token_id_feature)
+
+    def __getitem__(self,idx):
+        return self.token_id_feature[idx]
+
+    def text_to_token_id(self,df):
+        token_id=[]
+        for i in range(df.shape[0]):
+            data={}
+            label=df.loc[i,"label"]
+            text=df.loc[i,"sentence"]
+            data["text"]=text
+            data["label"]=torch.tensor(label,dtype=torch.float32)
+            ids=self.get_token_id(text)
+            if ids!=[]:
+                data["input_ids"]=torch.tensor(ids)
+                token_id.append(data)
+        return token_id
+
+    def get_token_id(self,text):
+        words=text.split(" ")
+        ids=[]
+        for word in words:
+            if word in token_to_id.keys():
+                id=token_to_id[word]
+                ids.append(id)
+        return ids
+
+    def add_average_vec(self,data_list):
+        for data in data_list:
+            ids=data["input_ids"]
+            vectors=[embedding_matrix[id] for id in ids]
+            average_vec=np.mean(vectors,axis=0)
+            data["feature_vec"]=torch.tensor(average_vec,dtype=torch.float32)
+        return data_list
+
+train_dataset=Dataset(train_df)
+train_dataloader=DataLoader(train_dataset,batch_size=64,shuffle=True)
+dev_dataset=Dataset(dev_df)
+dev_dataloader=DataLoader(dev_dataset,batch_size=64,shuffle=True)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using {} device'.format(device))
@@ -24,17 +79,12 @@ class NeuralNetwork(nn.Module):
         probs=torch.sigmoid(logits)
         return probs
 
-input_dim=embedding_matrix.shape[1]
-model = NeuralNetwork(input_dim).to(device)
-
 def show_result():
+    model = NeuralNetwork().to(device)
     print(model)
 
-#show_result()
+show_result()
 # 実行結果
 """
-Using cpu device
-NeuralNetwork(
-  (linear): Linear(in_features=300, out_features=1, bias=True)      
-)
+
 """
