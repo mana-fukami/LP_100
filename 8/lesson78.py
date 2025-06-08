0838d3deb5dbdde4f145cd15a0f92d0d612ac48a
mana-fukami <3kan.me6n@gmail.com>
2025-06-08 23:44:32 +0900

    before running

--- 8/lesson78.py
+++ 8/lesson78.py
@@ -1,5 +1,6 @@
 """
-問題76のモデル学習をGPU上で実行せよ。
+問題77の学習において、単語埋め込みのパラメータも同時に更新する
+ファインチューニングを導入せよ。
 また、学習したモデルの開発セットにおける正解率を求めよ。
 """
 import pandas as pd
@@ -20,13 +21,12 @@ dev_df=pd.read_csv(dev_tsv,sep="\t")
 class CustomDataset(Dataset):
     def __init__(self,data_df):
         token_id=self.text_to_token_id(data_df)
-        self.token_id_feature=self.add_average_vec(token_id)
 
     def __len__(self):
-        return len(self.token_id_feature)
+        return len(self.token_id)
 
     def __getitem__(self,idx):
-        return self.token_id_feature[idx]
+        return self.token_id[idx]
 
     def text_to_token_id(self,df):
         token_id=[]
@@ -51,26 +51,17 @@ class CustomDataset(Dataset):
                 ids.append(id)
         return ids
 
-    def add_average_vec(self,data_list):
-        for data in data_list:
-            ids=data["input_ids"]
-            vectors=[embedding_matrix[id] for id in ids]
-            average_vec=np.mean(vectors,axis=0)
-            data["feature_vec"]=torch.tensor(average_vec,dtype=torch.float32)
-        return data_list
-
 # カスタムコラート関数
 def collate(batch):
     # トークン列の長さでソートする
     batch.sort(key=lambda x:len(x["input_ids"]),reverse=True)
     labels = torch.stack([item["label"] for item in batch])
     input_ids = [item["input_ids"] for item in batch]
-    features=torch.stack([item["feature_vec"] for item in batch])
 
     # input_idsをパディングして同じ長さに揃える
     padded_input_ids = pad_sequence(input_ids, batch_first=True, padding_value=0)
 
-    return {"input_ids": padded_input_ids,"label": labels, "feature_vec": features}
+    return {"input_ids": padded_input_ids,"label": labels}
 
 train_dataset=CustomDataset(train_df)
 train_dataloader=DataLoader(train_dataset,batch_size=64,shuffle=True, collate_fn=collate)
@@ -81,17 +72,22 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using {} device'.format(device))
 
 class NeuralNetwork(nn.Module):
-    def __init__(self,input_dim):
+    def __init__(self,embedding_matrix):
         super(NeuralNetwork,self).__init__()
-        self.linear=nn.Linear(input_dim,1)
+        # 単語埋め込みのパラメーターも学習する
+        vocab_size,embedding_dim=embedding_matrix.shape
+        self.embedding=nn.Embedding(vocab_size,embedding_dim)
+        self.embedding.weight=nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
+        self.linear=nn.Linear(embedding_dim,1)
 
-    def forward(self,x):
-        logits=self.linear(x)
+    def forward(self,input_ids):
+        embedded=self.embedding(input_ids)
+        avg_vec=embedded.mean(dim=1)
+        logits=self.linear(avg_vec)
         probs=torch.sigmoid(logits)
         return probs
 
-input_dim=embedding_matrix.shape[1]
-model = NeuralNetwork(input_dim)
+model = NeuralNetwork(embedding_matrix)
 model=nn.DataParallel(model) # 並列処理ができるようにラップする
 model=model.to(device)
 
@@ -103,7 +99,7 @@ num_epochs=10
 for epoch in range(num_epochs):
     model.train()
     for batch in train_dataloader:
-        features=batch["feature_vec"].to(device)
+        features=batch["input_ids"].to(device)
         labels=batch["label"].to(device).view(-1,1) # ラベルの形状を[64,1]に変換。デフォルトが[64]になっている
 
         outputs=model(features)
@@ -122,7 +118,7 @@ total=0
 
 with torch.no_grad(): #計算の高速化のために勾配計算を無効にする
     for batch in dev_dataloader:
-        features=batch["feature_vec"]
+        features=batch["input_ids"].to(device)
         labels=batch["label"].to(device).view(-1,1)
 
         # モデルの出力を取得
