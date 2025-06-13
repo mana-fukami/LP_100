fd45bcae2c3f77539a6a22c3e162a1c3063c72d0
mana-fukami <3kan.me6n@gmail.com>
2025-06-13 17:28:42 +0900

    85の変更に合わせて実行結果を更新

--- 9/lesson86.py
+++ 9/lesson86.py
@@ -23,7 +23,7 @@ def show_result():
 #show_result()
 # 実行結果
 """
-{'text': ['hide new secretions from the parental units ', 'contains no wit , only labored gags ', 'that loves its characters and communicates something rather beautiful about human nature ', 'remains utterly satisfied to remain the same throughout '], 'label': tensor([0., 0., 1., 0.]), 'tokens': {'input_ids': tensor([[  101,  5342,  2047,  3595,  8496,  2013,  1996, 18643,  3197,   102,
+{'text': ['hide new secretions from the parental units ', 'contains no wit , only labored gags ', 'that loves its characters and communicates something rather beautiful about human nature ', 'remains utterly satisfied to remain the same throughout '], 'label': tensor([0, 0, 1, 0]), 'tokens': {'input_ids': tensor([[  101,  5342,  2047,  3595,  8496,  2013,  1996, 18643,  3197,   102,
              0,     0,     0,     0,     0],
         [  101,  3397,  2053, 15966,  1010,  2069,  4450,  2098, 18201,  2015,
            102,     0,     0,     0,     0],
@@ -41,30 +41,32 @@ def show_result():
 # 見やすく整えた
 """
 {
-    'text':['hide new secretions from the parental units ', 'contains no wit , only labored gags ', 'that loves its characters and communicates something rather beautiful about human nature ', 'remains utterly satisfied to remain the same throughout '],
-    'label': tensor([0., 0., 1., 0.]),
+    'text': [
+        'hide new secretions from the parental units ',
+        'contains no wit , only labored gags ',
+        'that loves its characters and communicates something rather beautiful about human nature ',
+        'remains utterly satisfied to remain the same throughout '
+    ],
+    'label': tensor([0, 0, 1, 0]),
     'tokens': {
-        'input_ids':
-            tensor([
-                [  101,  5342,  2047,  3595,  8496,  2013,  1996, 18643,  3197,   102,     0,     0,     0,     0,     0],
-                [  101,  3397,  2053, 15966,  1010,  2069,  4450,  2098, 18201,  2015,   102,     0,     0,     0,     0],
-                [  101,  2008,  7459,  2049,  3494,  1998, 10639,  2015,  2242,  2738,  3376,  2055,  2529,  3267,   102],
-                [  101,  3464, 12580,  8510,  2000,  3961,  1996,  2168,  2802,   102,     0,     0,     0,     0,     0]
-            ]),
-        'token_type_ids':
-            tensor([
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            ]),
-        'attention_mask':
-            tensor([
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
-            ])
+        'input_ids':tensor([
+            [  101,  5342,  2047,  3595,  8496,  2013,  1996, 18643,  3197,   102,     0,     0,     0,     0,     0],
+            [  101,  3397,  2053, 15966,  1010,  2069,  4450,  2098, 18201,  2015,   102,     0,     0,     0,     0],
+            [  101,  2008,  7459,  2049,  3494,  1998, 10639,  2015,  2242,  2738,  3376,  2055,  2529,  3267,   102],
+            [  101,  3464, 12580,  8510,  2000,  3961,  1996,  2168,  2802,   102,     0,     0,     0,     0,     0]
+        ]),
+        'token_type_ids': tensor([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]),
+        'attention_mask': tensor([
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+        ])
     }
 }
 """
